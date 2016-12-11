75cb3398c2d6cb4f1ba5f8df3c7949bfdf99ec5b
Ivan Grbavac <ivan.grbavac@live.com>
2016-12-11 21:56:23 +0100

    Trying apollo for graphql

--- client/app.js
+++ client/app.js
@@ -5,14 +5,28 @@ import { Provider } from 'react-redux';
 import dockerStore from './store/dockerStore';
 import Images from './components/Images';
 
+
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
+
+const client = new ApolloClient({
+  networkInterface: createNetworkInterface({ uri: 'http://localhost:4000' }),
+});
+
+import { ApolloProvider } from 'react-apollo';
+
+
+
+
 const App = () => (
-  <Provider store={dockerStore}>
-    <div>
-      <Router history={browserHistory}>
-        <Route path="/" component={Images} />
-      </Router>
-    </div>
-  </Provider>
+  <ApolloProvider client={client}>
+    <Provider store={dockerStore}>
+      <div>
+        <Router history={browserHistory}>
+          <Route path="/" component={Images} />
+        </Router>
+      </div>
+    </Provider>
+  </ApolloProvider>
 );
 
 ReactDOM.render(<App />, document.getElementById('docker-ui-app'));
